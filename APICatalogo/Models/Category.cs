98b481410ef606096a1b3654eb1a1c12f0ac3904
Wellz <owellingtondepaula@gmail.com>
2026-03-19 15:44:09 -0300

    Added Data Annotations

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categories")]
 public class Category {
     public Category() {
         Products = new Collection<Product>();
     }
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public ICollection<Product>? Products { get; set; }
 }
--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -1,12 +1,25 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace APICatalogo.Models;
+
+[Table("Products")]
 public class Product {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? Price { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
-    public float? Amount { get; set; }
+    public float Amount { get; set; }
     public DateTime RegistrationDate { get; set; }
     public int CategoryId { get; set; }
     public Category? Category { get; set; }
